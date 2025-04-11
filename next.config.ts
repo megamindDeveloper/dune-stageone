ec46e2f5720c712eb0706a7b0e4741ae78da4a15
Jamshadj <jamshadalij2004@gmail.com>
2025-04-11 17:17:02 +0530

    config added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, 
+  },
+  output: 'export',
+  
+  trailingSlash: true,
 };
 
 export default nextConfig;
